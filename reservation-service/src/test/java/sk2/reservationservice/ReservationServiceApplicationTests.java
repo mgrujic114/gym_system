4575852197115efa654b534a85eece840b2674f3
Milica Grujic <115893652+mgrujic114@users.noreply.github.com>
2024-01-11 22:18:58 +0100

    Add files via upload

---
+++ reservation-service/src/test/java/sk2/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sk2.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
