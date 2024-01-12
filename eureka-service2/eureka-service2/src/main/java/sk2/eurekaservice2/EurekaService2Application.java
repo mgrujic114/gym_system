b3bc40f1b5ef7272d3ad9a3d9b9a1507cd581331
Milica Grujic <115893652+mgrujic114@users.noreply.github.com>
2024-01-12 21:59:55 +0100

    Add files via upload

---
+++ eureka-service2/eureka-service2/src/main/java/sk2/eurekaservice2/EurekaService2Application.java
@@ -0,0 +1,13 @@
+package sk2.eurekaservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaService2Application.class, args);
+	}
+
+}
---
+++ eureka-service2/eureka-service2/src/test/java/sk2/eurekaservice2/EurekaService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sk2.eurekaservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
