c5df14685c272f76d7d46d17d1f651365125f4e0
Milica Grujic <mgrujic11422rn@raf.rs>
2024-01-12 23:37:22 +0100

    i ostali servisi povezivanje preko api gejtveja

--- training-service/src/main/java/sk2/trainingservice/dto/SessionDto.java
+++ training-service/src/main/java/sk2/trainingservice/dto/SessionDto.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-@Getter
-@Setter
+
 public class SessionDto {
 
     private Long id;
@@ -15,4 +14,36 @@ public class SessionDto {
     @JsonProperty("gym")
     private GymDto screenDto;
     private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TrainingDto getMovieDto() {
+        return movieDto;
+    }
+
+    public void setMovieDto(TrainingDto movieDto) {
+        this.movieDto = movieDto;
+    }
+
+    public GymDto getScreenDto() {
+        return screenDto;
+    }
+
+    public void setScreenDto(GymDto screenDto) {
+        this.screenDto = screenDto;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
